4ad25f713035f02e59ab43555601a49e39100163
IvanCardel <jics080493@hotmail.com>
2020-04-28 22:46:12 -0500

    mejora al footer y avance de 70%

--- layout/footer.php
+++ layout/footer.php
@@ -1,23 +1,44 @@
 <br>
 <br>
-<div class="container-fluid bg-primary text-white">
-    <div class="row pt-4">
-        <div class="col-6">
-            <h4>Acerca de</h4>
-            <p>Esto es un sitio web de un proyecto escolar, consiste en un menú de recetas en el que podemos almacenar nuestras recetas de una manera sencilla.</p>
-        </div>
-        <div class="col-6">
-            <h4>Contacto</h4>
-            <ul>
-                <li class="text-light"><a href="mailto:correo1@dominio.ext" class="correoFooter text-light"><i class="fas fa-envelope-open-text"></i>&nbsp jics080493@gmail.com</a></li>
-                <li class="text-light"><a href="tel:4612518801 text-light" class="text-light"><i class="fas fa-phone mr-3 text-light"></i> + 52 461 451 88 01</a></li>
-                <li class="text-light"><i class="fas fa-home mr-3"></i>Villa de Herón #105</li>
-            </ul>
-        </div>
-    </div>
+<!-- Footer -->
+<footer class="page-footer font-small bg-primary pt-4">
+  <div class="container-fluid text-center text-md-left">
     <div class="row">
-        <div class="col-12 text-center pt-2 pb-2">
-        <i class="far fa-copyright"></i>José Iván Cardel Sotelo
-        </div>
+      <div class="col-md-6 mt-md-0 mt-3">
+          <div class="container">
+          <h5 class="text-uppercase text-light">Acerca de</h5>
+        <p class="text-light" style="line-height: 2rem">
+        Esto es un sitio web de un proyecto escolar, consiste en un menú de recetas en el que podemos 
+        almacenar nuestras recetas de una manera sencilla. Tú puedes ser un usuario si lo quisieras, 
+        solamente debes de crear una cuenta y listo, participas de una manera muy sencilla.</p>
+          </div>
+      </div>
+      <hr class="clearfix w-100 d-md-none pb-3">
+      <div class="col-md-3 mb-md-0 mb-3">
+        <h5 class="text-uppercase text-light">Redes Sociales</h5>
+        <ul class="list-unstyled" style="line-height: 3rem">
+          <li>
+          <a class="correoFooter text-light"><i class="fab fa-facebook-f fa-lg text-light mr-4"> </i></a>
+          </li>
+          <li>
+          <a class="correoFooter text-light"><i class="fab fa-twitter fa-lg text-light mr-4"> </i></a>
+          </li>
+          <li>
+          <a class="correoFooter text-light"><i class="fab fa-instagram fa-lg text-light mr-4"> </i></a>
+          </li>
+        </ul>
+      </div>
+    
+      <div class="col-md-3 mb-md-0 mb-3">
+        <h5 class="text-uppercase text-light">Contacto</h5>
+        <ul class="list-unstyled" style="line-height: 3rem">
+          <li class="text-light"><a href="tel:4612518801 text-light" class="correoFooter text-light"><i class="fas fa-phone mr-3 text-light"></i> + 52 461 451 88 01</a></li>
+          <li class="text-light"><a href="mailto:correo1@dominio.ext" class="correoFooter text-light"><i class="fas fa-envelope-open-text"></i>&nbsp jics080493@gmail.com</a></li>
+          <li class="text-light"><i class="fas fa-home mr-3"></i>Villa de Herón #105</li>
+        </ul>
+      </div>
     </div>
-</div>
+  </div>
+  <div class="footer-copyright text-center text-white foo py-3">© 2020 Copyright: José Iván Cardel Sotelo 
+  </div>
+</footer>
